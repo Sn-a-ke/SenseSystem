5ecae1e017f6e9e8d3a41484eb5777187811986a
Oleksandr Marchenko <epoly.design@gmail.com>
2023-04-07 19:30:51 +0300

    [FIX] errror:  error: '&&' within '||'

--- SenseSystem/Source/SenseSystem/Private/SenseStimulusBase.cpp
+++ SenseSystem/Source/SenseSystem/Private/SenseStimulusBase.cpp
@@ -270,7 +270,7 @@ void USenseStimulusBase::OnComponentDestroyed(const bool bDestroyingHierarchy)
 void USenseStimulusBase::SetComponentTickEnabled(const bool bTickEnabled)
 {
 	ResetDelayTickStart();
-	if (bEnable || !bTickEnabled && !bEnable)
+	if (bEnable || !bTickEnabled)
 	{
 		Super::SetComponentTickEnabled(bTickEnabled);
 	}
